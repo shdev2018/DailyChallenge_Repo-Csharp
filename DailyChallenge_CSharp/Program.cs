fb5f54f2910995ed4d6c9df7c1b4e3ce8a392a36
shdev2018 <shaunhall.dev@gmail.com>
2020-07-07 17:15:10 +0100

    07/07/2020

--- DailyChallenge_CSharp/Program.cs
+++ DailyChallenge_CSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace DailyChallenge_CSharp
@@ -7,11 +8,49 @@ namespace DailyChallenge_CSharp
     {
         static void Main(string[] args)
         {
-            bool result = prob_06_07_2020(new List<int> { 10, 15, 3, 7}, 17);
-            Console.WriteLine(result);
+            var result = prob_07_07_2020(new List<int> { 3, 2, 1 });
+            Console.WriteLine(JsonConvert.SerializeObject(result));
+
+            //var result = prob_06_07_2020(new List<int> { 10, 15, 3, 7}, 17);
         }
 
-        // Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+        /// Given an array of integers, return a new array such that each element at index i of the new 
+        /// array is the product of all the numbers in the original array except the one at i.
+        private static IList<int> prob_07_07_2020(IList<int> array)
+        {
+            List<int> newArray = new List<int>();
+            int j;
+            for (int i = 0; i < array.Count; i++)
+            {
+                int temp = 0;
+                for (j = 0; j < array.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        if (temp == 0)
+                        {
+                            temp = array[j];
+                        }
+                        else
+                        {
+                            temp = temp * array[j];
+                        }
+                    }
+                }
+                if (j == i)
+                {
+                    newArray.Add(array[j]);
+                }
+                else
+                {
+                    newArray.Add(temp);
+                }
+            }
+            return newArray;
+        }
+        // Complete on 00:11:20
+
+        /// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
         private static bool prob_06_07_2020 (IList<int> list, int k)
         {
             for (int i = 0; i < list.Count; i++)
@@ -29,5 +68,7 @@ namespace DailyChallenge_CSharp
             return false;
         }
         // Completed in 00:04:37
+
+        
     }
 }
