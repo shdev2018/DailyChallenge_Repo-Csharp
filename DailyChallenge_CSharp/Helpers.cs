295529c1225fa93e621bc315c004972d00510c8d
shdev2018 <shaunhall.dev@gmail.com>
2020-07-11 21:54:26 +0100

    11/07/2020

---
+++ DailyChallenge_CSharp/Helpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyChallenge_Csharp
+{
+    class Helpers
+    {
+        public class MemoryMimic<T>
+        {
+            public List<Node<T>> _nodes;
+            public List<int> _availIndexes;
+            public Random rand;
+
+            public MemoryMimic()
+            {
+                _nodes = new List<Node<T>>();
+                _availIndexes = new List<int>();
+                rand = new Random();
+                AddSpace();
+            }
+
+            private void AddSpace()
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    _nodes.Add(null);
+                    _availIndexes.Add(_nodes.Count);
+                }
+            }
+
+            public int Add(Node<T> node)
+            {
+                var availIndex = rand.Next(_availIndexes.Count);
+                var i = _availIndexes[availIndex];
+                _nodes[i - 1] = node;
+                _availIndexes.Remove(i);
+                if (_availIndexes.Count == 0) AddSpace();
+                return i;
+            }
+
+            public Node<T> this[int index]
+            {
+                get { return _nodes[index - 1]; }
+                set { _nodes[index - 1] = value; }
+            }
+        }
+    }
+}
--- DailyChallenge_CSharp/Program.cs
+++ DailyChallenge_CSharp/Program.cs
@@ -5,17 +5,143 @@ using System.Linq;
 
 namespace DailyChallenge_CSharp
 {
-    class Program
+    unsafe class Program
     {
         static void Main(string[] args)
         {
-            var result = prob_09_07_2020(new List<int> { 1, 2, 0 });
+            var result = prob_11_07_2020();
             #region OldProbs
+            //var result = prob_09_07_2020(new List<int> { 1, 2, 0 });
             //var result = prob_07_07_2020(new List<int> { 3, 2, 1 });
             //var result = prob_06_07_2020(new List<int> { 10, 15, 3, 7}, 17);
             #endregion
-            Console.WriteLine(JsonConvert.SerializeObject(result));
+            Console.WriteLine(JsonConvert.SerializeObject(result.Get(2)));
         }
+
+
+
+        /// An XOR linked list is a more memory efficient doubly linked list. Instead of each node holding next 
+        /// and prev fields, it holds a field named both, which is an XOR of the next node and the previous node. 
+        /// Implement an XOR linked list; it has an add(element) which adds the element to the end, and a 
+        /// get(index) which returns the node at index.
+        private static XorList<int> prob_11_07_2020()
+        {
+            var list = new XorList<int>();
+            list.Add(5);
+            list.Add(7);
+            list.Add(4);
+            list.Add(36);
+            list.Add(12);
+            return list;
+        }
+        public class XorList<T>
+        {
+            private int HeadIndex { get; set; }
+            private MemoryMimic<T> _memory;
+
+            public XorList()
+            {
+                _memory = new MemoryMimic<T>();
+            }
+
+            public void Add(T element)
+            {
+                if (HeadIndex == 0)
+                {
+                    HeadIndex = _memory.Add(new Node<T>(element));
+                    return;
+                }
+
+                var currentPoint = 0;
+                var nextPoint = HeadIndex;
+                var prevPoint = 0;
+                Node<T> currentNode;
+
+                do
+                {
+                    currentNode = _memory[nextPoint];
+                    prevPoint = currentPoint;
+                    currentPoint = nextPoint;
+                    nextPoint = currentNode.NextPrevValue ^ prevPoint;
+                } while (nextPoint != 0);
+
+                var newIndex = _memory.Add(new Node<T>(element));
+                var newNode = _memory[newIndex];
+                currentNode.NextPrevValue = prevPoint ^ newIndex;
+                newNode.NextPrevValue = currentPoint;
+            }
+
+            public Node<T> Get(int index)
+            {
+                if (index < 0 || HeadIndex == 0) return null;
+                var currentPoint = 0;
+                var nextPoint = HeadIndex;
+                var prevPoint = 0;
+                Node<T> currentNode;
+                var i = 0;
+
+                do
+                {
+                    currentNode = _memory[nextPoint];
+                    prevPoint = currentPoint;
+                    currentPoint = nextPoint;
+                    nextPoint = currentNode.NextPrevValue ^ prevPoint;
+                    i++;
+                } while (nextPoint != 0 && i <= index);
+
+                if (nextPoint == 0 && (i - 1) != index) return null;
+
+                return currentNode;
+            }
+        }
+        public class Node<T>
+        {
+            public T Data { get; set; }
+            public int NextPrevValue { get; set; }
+
+            public Node(T data, int nextPrevValue = 0) { Data = data; NextPrevValue = nextPrevValue; }
+            public override string ToString() { return Data.ToString(); }
+        }
+        public class MemoryMimic<T>
+        {
+            public List<Node<T>> _nodes;
+            public List<int> _availIndexes;
+            public Random rand;
+
+            public MemoryMimic()
+            {
+                _nodes = new List<Node<T>>();
+                _availIndexes = new List<int>();
+                rand = new Random();
+                AddSpace();
+            }
+
+            private void AddSpace()
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    _nodes.Add(null);
+                    _availIndexes.Add(_nodes.Count);
+                }
+            }
+
+            public int Add(Node<T> node)
+            {
+                var availIndex = rand.Next(_availIndexes.Count);
+                var i = _availIndexes[availIndex];
+                _nodes[i - 1] = node;
+                _availIndexes.Remove(i);
+                if (_availIndexes.Count == 0) AddSpace();
+                return i;
+            }
+
+            public Node<T> this[int index]
+            {
+                get { return _nodes[index - 1]; }
+                set { _nodes[index - 1] = value; }
+            }
+        }
+        // Unable to solve, solution found at https://dotnetfiddle.net/uxBAzM and analyzed
 
         /// Given an array of integers, find the first missing positive integer in linear time and constant space. 
         /// In other words, find the lowest positive integer that does not exist in the array. 
